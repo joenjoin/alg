bd5bf8ee463b123b6cd46379c364448903a11ff9
Cheng <joenjoin@gmail.com>
2015-12-06 21:21:34 +0800

    Division without using operator "/"

---
+++ src/tidbits/Division.java
@@ -0,0 +1,21 @@
+package tidbits;
+
+/**
+ * Implement integer division without using operator "/"
+ */
+public class Division {
+
+    /**
+     * Implement integer division using binary search
+     */
+    public int divideBinarySearch(int dividend, int divisor) {
+        return 0;
+    }
+
+    /**
+     * https://en.wikipedia.org/wiki/Division_algorithm
+     */
+    public int divide(int dividend, int divisor) {
+        return 0;
+    }
+}
