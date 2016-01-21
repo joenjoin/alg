96cf6fdba9b62eb9d7bbf488f6e59057fed25198
joenjoin <joenjoin@gmail.com>
2016-01-21 14:10:31 +0800

    Add string and dynamic programming packages

---
+++ src/me/tidbits/string/StringUtil.java
@@ -0,0 +1,33 @@
+package me.tidbits.string;
+
+public class StringUtil {
+
+	/**
+	 * 
+	 * Boundary conditions to handle:
+	 * 
+	 * @Input "   12 34"
+	 * @Output 12
+	 * 
+	 * @Input "+12"
+	 * @Output 12
+	 * 
+	 * @Input "-12"
+	 * @Output -12
+	 * 
+	 * @Input "--12"
+	 * @Output 0
+	 * 
+	 * @Input "++12"
+	 * @Output 0
+	 * 
+	 * @Input "0xa12"
+	 * @Output "0"
+	 * 
+	 * @param str
+	 * @return
+	 */
+	public int myAtoi(String str) {
+		return 0;
+	}
+}
